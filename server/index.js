55fcf8cec035ac7dc8ef9425038cb59fab7ae663
Dillon Benson <dibenso93@gmail.com>
2021-04-11 23:30:10 -0400

    use port 3001

--- server/index.js
+++ server/index.js
@@ -137,7 +137,7 @@ const server = new ApolloServer({
     return { user: decodedToken };
   }
 });
-const port = parseInt(process.env.PORT, 10) || 3000;
+const port = parseInt(process.env.PORT, 10) || 3001;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
